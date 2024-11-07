a205e0dade6e2796f5f7fe547f3efcfc5b18f9e8
Carson <jackparadise520a@gmail.com>
2024-11-07 16:17:32 +0800

    可以正式監聽剪貼簿的內容; 並且詢問是否保存圖片

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ package main
 #cgo windows LDFLAGS: -lgdi32
 #cgo CFLAGS: -I./csrc
 #include "clipboard_helper.c"
+
+// WNDPROC myDefWindowProcW = DefWindowProcW;
+
+// 定義一個簡單的窗口過程
+LRESULT CALLBACK MyWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    // if (uMsg == WM_CLIPBOARDUPDATE) {
+    //     PostMessage(hwnd, WM_USER + 1, 0, 0);
+    // }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+WNDPROC GetMyWindowProc() {
+    return MyWindowProc;
+}
 */
 import "C"
 import (
@@ -16,14 +30,9 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"syscall"
 	"unsafe"
 )
-
-var clipboardChanged chan bool
-
-func init() {
-	clipboardChanged = make(chan bool)
-}
 
 func EmptyClipboard() {
 	if C.OpenClipboard(C.HWND(C.NULL)) != 0 {
@@ -88,8 +97,13 @@ func saveClipboardImage(outputDir string) error {
 	var outputPath string
 	fmt.Println("請輸入文件名（不包括路徑，將保存於指定目錄）：")
 	if _, err := fmt.Scanln(&outputPath); err != nil {
+		if err.Error() == "unexpected newline" {
+			log.Printf("檔名為空，跳過不存檔")
+			return nil
+		}
 		return err
 	}
+
 	if filepath.Ext(outputPath) == "" {
 		outputPath += ".bmp"
 	}
@@ -123,13 +137,67 @@ func saveClipboardImage(outputDir string) error {
 	}
 	absOutputPath, _ := filepath.Abs(outputPath)
 	log.Printf("圖片已順利保存至:%q", absOutputPath)
-	EmptyClipboard()
+	// EmptyClipboard()
 
 	return nil
 }
 
+var clipboardChanged chan bool
+
 func init() {
 	print(app.About())
+}
+
+func AddClipboardFormatListener() error {
+	// 創建一個隱藏的窗口
+
+	// 轉換 Go 字符串到 C 字符串
+	className, _ := syscall.UTF16PtrFromString("clipboard-img-saver")
+	cClassName := C.CString(C.GoString((*C.char)(unsafe.Pointer(className))))
+	defer C.free(unsafe.Pointer(cClassName))
+
+	hInstance := C.GetModuleHandle(nil)
+
+	var wc C.WNDCLASSW
+	// wc.lpfnWndProc = C.DefWindowProcW  // cannot use (_Cgo_ptr(_Cfpvar_fp_DefWindowProcW)) (value of type unsafe.Pointer) as _Ctype_WNDPROC value in assignment
+	// wc.lpfnWndProc = C.myDefWindowProcW // 使用聲明的 myDefWindowProcW
+	// wc.lpfnWndProc = C.MyWindowProc // cannot use (_Cgo_ptr(_Cfpvar_fp_MyWindowProc)) (value of type unsafe.Pointer) as _Ctype_WNDPROC value in assignment
+	wc.lpfnWndProc = C.GetMyWindowProc()
+	wc.cbClsExtra = 0
+	wc.cbWndExtra = 0
+	wc.hInstance = hInstance
+	wc.hIcon = nil
+	wc.hIcon = nil
+	wc.hCursor = nil
+	wc.hbrBackground = nil
+	wc.lpszMenuName = nil
+	// wc.lpszClassName = (*C.wchar_t)(unsafe.Pointer(className))  // runtime error: cgo argument has Go pointer to unpinned Go pointer  Go 指針不應該直接傳遞給 C 語言函數（在這種情況下是 C.RegisterClassW），因為 Go 垃圾回收器（GC）可能會移動這些指針。這在 cgo 中是一個常見問題，當 Go 指針包含 Go 分配的記憶體時，不能直接將其傳遞給 C 函數. 若 className 是單純的字符串，可以將其轉換成 C 字符串並在用完後釋放，這樣可以避免 Go 指針被傳遞給 C 函數
+	wc.lpszClassName = (*C.wchar_t)(unsafe.Pointer(cClassName))
+
+	if C.RegisterClassW(&wc) == 0 {
+		return fmt.Errorf("failed to register window class")
+	}
+
+	hwnd := C.CreateWindowExW(
+		0,
+		(*C.wchar_t)(unsafe.Pointer(cClassName)), // 使用 UTF16 字串指標
+		nil,
+		0,
+		0, 0, 0, 0,
+		nil,
+		nil,
+		nil,
+		nil,
+	)
+	if hwnd == nil {
+		return fmt.Errorf("failed to create window")
+	}
+
+	success := C.AddClipboardFormatListener(hwnd)
+	if success == 0 {
+		return fmt.Errorf("無法添加剪貼簿監聽")
+	}
+	return nil
 }
 
 func main() {
@@ -141,8 +209,51 @@ func main() {
 		return
 	}
 	fmt.Printf("圖片輸出目錄: %s\n", outputDir)
-	err := saveClipboardImage(outputDir)
+
+	clipboardChanged = make(chan bool)
+
+	// 註冊剪貼簿監聽
+	go func() {
+		for {
+			select {
+			case <-clipboardChanged:
+				err := saveClipboardImage(outputDir)
+				if err != nil {
+					log.Printf("錯誤: %s\n", err) // 如果有錯誤可能就會卡住，收不到下一個GetMessage的消息. 可能是stdout有衝突?
+				}
+			}
+		}
+	}()
+
+	// 不能拿其他的窗口，要自己建立，否則GetMessage還是拿不到東西
+	// windowName, _ := syscall.UTF16PtrFromString("MSPaintApp")
+	// paintWindowHWND := C.FindWindowW((*C.wchar_t)(unsafe.Pointer(windowName)), (*C.wchar_t)(nil))
+	// C.AddClipboardFormatListener(C.HWND(C.NULL))
+	// success := C.AddClipboardFormatListener(paintWindowHWND)
+	// if success == 0 {
+	// 	fmt.Println("無法添加剪貼簿監聽於MSPaintApp")
+	// 	return
+	// }
+
+	err := AddClipboardFormatListener()
 	if err != nil {
-		log.Printf("錯誤: %s\n", err)
+		log.Fatal(err)
+	}
+	fmt.Println("成功設置剪貼簿監聽")
+
+	var msg C.MSG
+	for {
+		ret := C.GetMessage(&msg, nil, 0, 0) // 使用GetMessage之前一定要先創建一個窗口，不然執行到這邊就會被卡住
+		// ret := C.GetMessage(&msg, paintWindowHWND, 0, 0)
+		if ret <= 0 {
+			break
+		}
+		if msg.message == C.WM_CLIPBOARDUPDATE {
+			clipboardChanged <- true
+			log.Println("檢測到剪貼簿變更")
+			// continue // 與這個無關，假設有錯誤加了這個還是會卡住
+		}
+		C.TranslateMessage(&msg)
+		C.DispatchMessage(&msg)
 	}
 }
