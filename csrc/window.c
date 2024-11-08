34641916a5eb9c24bf50bb051fc5fdd319d0ec8d
Carson <jackparadise520a@gmail.com>
2024-11-08 14:23:31 +0800

    僅在剪貼簿有圖片時候才會觸發WM_IMG_SAVE

--- csrc/window.c
+++ csrc/window.c
@@ -5,7 +5,25 @@ const UINT WM_IMG_SAVE = WM_USER + 1;
 // 定義一個簡單的窗口過程
 LRESULT CALLBACK MyWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     if (uMsg == WM_CLIPBOARDUPDATE) {
-        PostMessage(hwnd, WM_IMG_SAVE, 0, 0);
+        // 打開剪貼簿並檢查是否包含圖片格式
+        if (OpenClipboard(NULL)) {
+            UINT format = EnumClipboardFormats(0);
+            BOOL hasImage = FALSE;
+
+            // 遍歷剪貼簿格式，檢查是否包含圖片格式
+            while (format != 0) {
+                if (format == CF_BITMAP || format == CF_DIB || format == CF_DIBV5) {
+                    hasImage = TRUE;
+                    break;
+                }
+                format = EnumClipboardFormats(format);
+            }
+            CloseClipboard();
+
+            if (hasImage) {
+                PostMessage(hwnd, WM_IMG_SAVE, 0, 0);
+            }
+        }
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
